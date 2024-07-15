ccd5b71d043f417fe700b70411531743c1cd8e03
SakilMondal <mondalsakil@gmail.com>
2024-07-15 23:08:47 +0530

    Revert "whyred: Build libldacBT_bco.so"
    This reverts commit e6ecfabd5b9622203a1a42843cf435a5383f42f8.
    
    Change-Id: I1c812c57499596a20c3f4f0bec62378d887a2c0c

--- device.mk
+++ device.mk
@@ -117,8 +117,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
-    libldacBT_bco
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
